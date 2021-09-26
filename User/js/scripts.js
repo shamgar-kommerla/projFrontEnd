f08ef8868f63770210c806094aa6d06226dce2f9
shamgar-kommerla <shamgar.kommerla@gmail.com>
2021-09-27 00:03:54 +0530

    Design non-responsive

---
+++ User/js/scripts.js
@@ -0,0 +1,26 @@
+const hamb = document.getElementById('hamburger');
+const navBg = document.getElementById('nav-bg');
+const navList = document.getElementById('nav-list');
+// console.log(hamb.children);
+hamb.addEventListener('click', ()=> {
+    // console.log('clicked');
+    if(hamb.children[0].classList.length<=1){
+        hamb.children[0].classList.add('bar1-change');
+        hamb.children[1].classList.add('bar2-change');
+        hamb.children[2].classList.add('bar3-change');
+        // navBg.classList.add('nav-bg-change');
+        // setTimeout(()=>{
+        navList.classList.add('nav-list-change');
+        // },300)
+    }else{
+        hamb.children[0].classList.remove('bar1-change');
+        hamb.children[1].classList.remove('bar2-change');
+        hamb.children[2].classList.remove('bar3-change');
+        setTimeout(() => {
+            navList.classList.remove('nav-list-change');
+        },300);
+        
+    }
+    
+})
+
